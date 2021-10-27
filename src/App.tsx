a57fff94ea405031058fb3259f3d2bfc1219bf2c
Julio Balbino <julio4110@hotmail.com>
2021-10-27 15:10:48 -0300

    Navbar e footer com material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import Home from './paginas/home/Home';
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
+import { Grid } from "@material-ui/core";
 
 function App() {
   return (
-    <Home/>
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
